24d0c52a3b3c82e72ade414b3b7394ab3ecd73ed
Ruben <ruben@urano.group>
2023-01-05 08:24:40 -0500

    update aspect log

--- core-aspect/src/main/java/pe/com/cronos/core/aspect/log/LogExecutionAspect.java
+++ core-aspect/src/main/java/pe/com/cronos/core/aspect/log/LogExecutionAspect.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
 
 @Slf4j
 @Aspect
@@ -11,12 +12,14 @@ public class LogExecutionAspect {
 
     @Around("@annotation(pe.com.cronos.core.aspect.log.LogExecution)")
     public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
-        String className = joinPoint.getTarget().getClass().getCanonicalName();
-        String methodName = joinPoint.getSignature().getName();
+        MethodSignature methodSig = (MethodSignature) joinPoint.getSignature();
+
+        String className = joinPoint.getTarget().getClass().getSimpleName();
+        String methodName = methodSig.getName();
         try {
-            log.info("Start, {}.{}", className, methodName);
+            log.info("Start, {}.{} <- {}", className, methodName, methodSig.getParameterNames());
             Object response = joinPoint.proceed();
-            log.info("End, {}.{}", className, methodName);
+            log.info("End, {}.{} -> {}", className, methodName, response.getClass().getSimpleName());
             return response;
         } catch (Throwable e) {
             log.error("Error, {}.{}", className, methodName);
