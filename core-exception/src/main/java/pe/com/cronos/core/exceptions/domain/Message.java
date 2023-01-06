af6fcddc104de6cf962697a88aa454e22254baec
Ruben <ruben@urano.group>
2023-01-05 23:32:16 -0500

    update error message

--- core-exception/src/main/java/pe/com/cronos/core/exceptions/domain/Message.java
+++ core-exception/src/main/java/pe/com/cronos/core/exceptions/domain/Message.java
@@ -1,13 +1,13 @@
 package pe.com.cronos.core.exceptions.domain;
 
 public enum Message {
-    CORE_TOKEN_CREATION(Group.CORE_TOKEN, "C-T-0001", "Error in token creation", DefaultMessage.CORE_ERROR),
-    CORE_TOKEN_VALIDATION(Group.CORE_TOKEN, "C-T-0002", "Error in token validation", DefaultMessage.CORE_ERROR),
-    CORE_CRYPTO_DIGEST(Group.CORE_CRYPTO, "C-C-0001", "Error in create digest", DefaultMessage.CORE_ERROR),
-    CRONOS_AUTH_MS_LOGIN_NOT_EXISTS(Group.CRONOS_AUTH_MS, "MS-AUTH-0001", "User mot exists", DefaultMessage.CORE_ERROR),
-    CRONOS_AUTH_MS_LOGIN_FAILED(Group.CRONOS_AUTH_MS, "MS-AUTH-0001", "Error in login", DefaultMessage.CORE_ERROR),
-    CRONOS_AUTH_MS_LOGIN_USER_DISABLED(Group.CRONOS_AUTH_MS, "MS-AUTH-0001", "User account disabled", DefaultMessage.CORE_ERROR),
-    CRONOS_AUTH_MS_LOGIN_USER_EXPIRED(Group.CRONOS_AUTH_MS, "MS-AUTH-0001", "User account expired", DefaultMessage.CORE_ERROR);
+    CORE_TOKEN_CREATION(Group.CORE_TOKEN, "C-T-1001", "Error in token creation", DefaultMessage.CORE_ERROR),
+    CORE_TOKEN_VALIDATION(Group.CORE_TOKEN, "C-T-1002", "Error in token validation", DefaultMessage.CORE_ERROR),
+    CORE_CRYPTO_DIGEST(Group.CORE_CRYPTO, "C-C-1003", "Error in create digest", DefaultMessage.CORE_ERROR),
+    AUTH_MS_LOGIN_NOT_EXISTS(Group.CRONOS_AUTH_MS, "AUTH-MS-1001", "User mot exists", DefaultMessage.CORE_ERROR),
+    AUTH_MS_LOGIN_FAILED(Group.CRONOS_AUTH_MS, "AUTH-MS-1002", "Error in login", DefaultMessage.CORE_ERROR),
+    AUTH_MS_LOGIN_USER_DISABLED(Group.CRONOS_AUTH_MS, "AUTH-MS-1003", "User account disabled", DefaultMessage.CORE_ERROR),
+    AUTH_MS_LOGIN_USER_EXPIRED(Group.CRONOS_AUTH_MS, "AUTH-MS-1004", "User account expired", DefaultMessage.CORE_ERROR);
 
     private Group group;
     private String code;
