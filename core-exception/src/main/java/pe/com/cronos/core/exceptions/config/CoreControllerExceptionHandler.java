d91fb2451e2da1499b6ca6f8da4d2765c6483f48
Ruben <ruben@urano.group>
2023-01-06 20:15:07 -0500

    remove package

--- core-exception/src/main/java/pe/com/cronos/core/exceptions/config/CoreControllerExceptionHandler.java
+++ core-exception/src/main/java/pe/com/cronos/core/exceptions/config/CoreControllerExceptionHandler.java
@@ -5,7 +5,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
 import pe.com.cronos.core.exceptions.CronosException;
 
 import javax.servlet.http.HttpServletRequest;
